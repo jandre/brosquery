3dc5c59396678f180e8a290d865fc5b50ecd4766
Jen Andre <jandre@gmail.com>
2015-04-27 08:33:40 -0400

    uncrustified

--- src/bro_module.cpp
+++ src/bro_module.cpp
@@ -1,95 +1,94 @@
-/*
- *  Copyright (c) 2014, Facebook, Inc.
- *  All rights reserved.
- *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant
- *  of patent rights can be found in the PATENTS file in the same directory.
- *
- */
-
 #include <osquery/sdk.h>
 #include "bro_table.h"
 
 using namespace osquery;
 
-class BroTable: public tables::TablePlugin {
-
-  private:
-    fs::path logPath_;
+class
+BroTable: public tables::TablePlugin {
+private:
+    fs::path             logPath_;
     tables::TableColumns columns_;
-    BroHeader header_;
+    BroHeader            header_;
 
-    void readRows(QueryData &results) {
-      if (fs::exists(logPath_)) {
-        std::ifstream fin(logPath_.string());
+    void
+    readRows(QueryData &results) {
+        if (fs::exists(logPath_)) {
+            std::ifstream fin(logPath_.string());
 
-        std::string line;
-        while (std::getline(fin, line)) {
-          boost::trim(line);
-          if (!line.size()) {
-            continue;
-          }
-          if (line.at(0) != '#') {
-            header_.parse(line, results);
-          }
+            std::string   line;
+            while (std::getline(fin, line)) {
+                boost::trim(line);
+                if (!line.size()) {
+                    continue;
+                }
+                if (line.at(0) != '#') {
+                    header_.parse(line, results);
+                }
+            }
         }
-      }
     }
 
-    void readColumns() {
-      columns_ = header_.tableColumns();
+    void
+    readColumns() {
+        columns_ = header_.tableColumns();
     }
 
-  public:
+public:
 
     BroTable() {
-      columns_ = {};
+        columns_ = {};
     }
 
-    void setTable(BroHeader &header, fs::path &logPath) {
-      header_ = header;
-      logPath_ = logPath;
-      readColumns();
+    void
+    setTable(BroHeader &header, fs::path &logPath) {
+        header_  = header;
+        logPath_ = logPath;
+        readColumns();
     }
 
-    tables::TableColumns columns() const {
-      return columns_;
+    tables::TableColumns
+    columns() const {
+        return columns_;
     }
 
-    QueryData generate(tables::QueryContext& request) {
-      QueryData results;
-      readRows(results);
-      return results;
+    QueryData
+    generate(tables::QueryContext& request) {
+        QueryData results;
+
+        readRows(results);
+        return results;
     }
 };
 
 CREATE_MODULE("bro", "0.0.1", "0.0.0");
 
-void initModule(void) {
+void
+initModule(void) {
+    fs::path logsPath = detectBroLogsPath();
 
-  fs::path logsPath = detectBroLogsPath();
-
-  if (fs::exists(logsPath) && fs::is_directory(logsPath)) {
-
-    fs::directory_iterator end;
-    for(fs::directory_iterator dir_iter(logsPath); dir_iter != end; ++dir_iter)
-    {
-      if (fs::is_regular_file(dir_iter->status()) &&
-          (dir_iter->path().extension() == ".log")) {
-
-        auto tableName = std::string("bro_") + dir_iter->path().stem().string();
-        fs::path tablePath = dir_iter->path();
-
-        BroHeader header;
-        if (header.read(tablePath)) {
-          REGISTER_MODULE(BroTable, "table", tableName.c_str());
-          auto table = std::dynamic_pointer_cast<BroTable>(
-              Registry::get("table", tableName.c_str()));
-          table->setTable(header, tablePath);
-          // fprintf(stderr, "XXX: bro module loaded %s\n", tableName.c_str());
-        }
-      }
+    if (!fs::exists(logsPath) || !fs::is_directory(logsPath)) {
+        /* no logs lodaed */
+        return;
     }
-  }
+    fs::directory_iterator end;
+
+    for (fs::directory_iterator dir_iter(logsPath); dir_iter != end; ++dir_iter) {
+        if (fs::is_regular_file(dir_iter->status()) && (dir_iter->path().extension() == ".log")) {
+            auto      tableName = std::string("bro_") + dir_iter->path().stem().string();
+            fs::path  tablePath = dir_iter->path();
+
+            BroHeader header;
+            if (header.read(tablePath)) {
+                REGISTER_MODULE(BroTable, "table", tableName.c_str());
+
+                /* this is a hack -- since I can't register instatiated objects, only */
+                /* table types, it's hard for me to dynamically set table columns or */
+                /* other structures. */
+                auto table = std::dynamic_pointer_cast < BroTable > (
+                    Registry::get("table", tableName.c_str()));
+                table->setTable(header, tablePath);
+            }
+        }
+    }
 }
+
--- src/bro_table.h
+++ src/bro_table.h
@@ -4,58 +4,59 @@
 #include <boost/algorithm/string/predicate.hpp>
 #include <boost/algorithm/string/replace.hpp>
 
-namespace fs = boost::filesystem;
-namespace pt = boost::property_tree;
+namespace       fs = boost::filesystem;
+namespace       pt = boost::property_tree;
 
 using namespace osquery;
 
-// The path to the default bro logs folder.
-// You can re-set this using -DBRO_LOGS_FOLDER=... in cmake
+/*
+ * The path to the default bro logs folder.
+ * You can re-set this using -DBRO_LOGS_FOLDER=... in cmake
+ */
 #ifndef DEFAULT_BRO_LOGS_FOLDER
 #define DEFAULT_BRO_LOGS_FOLDER "/opt/bro/logs"
 #endif
 
 fs::path detectBroLogsPath();
 
-class BroField {
-  public:
+class    BroField {
+public:
     std::string name_;
-    int position_;
+    int         position_;
     std::string type_;
     std::string tableType_;
 
-    BroField(std::string &name, int pos, std::string &type) {
-      name_ = name;
-      boost::replace_all(name_, ".", "_");
-      position_ = pos;
-      type_ = type;
+    BroField(std::string & name, int pos, std::string & type) {
+        name_     = name;
+        boost::replace_all(name_, ".", "_");
+        position_ = pos;
+        type_     = type;
     }
-    
-    void setType(std::string);
+
+    void        setType(std::string);
     std::string tableType() const;
     std::string name() const;
-
 };
 
 class BroHeader {
-
-  private:
-    bool ready_ = false;
+private:
+    bool        ready_ = false;
     std::string unset_field_;
     std::string empty_field_;
 
-    void readFields(std::string &input);
-    void readTypes(std::string &input);
+    void        readFields(std::string &input);
+    void        readTypes(std::string &input);
 
-  public:
-    std::vector<BroField> fields;
+public:
+    std::vector < BroField > fields;
     std::string separator;
 
-    BroHeader() : ready_(false) {}
+    BroHeader() : ready_(false) {
+    }
 
-    bool read(fs::path&);
-    void readHeader(std::string &);
-    void parse(std::string &, QueryData &);
+    bool                 read(fs::path&);
+    void                 readHeader(std::string &);
+    void                 parse(std::string &, QueryData &);
     tables::TableColumns tableColumns() const;
 };
 
